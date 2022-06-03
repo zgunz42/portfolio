30fd876b12680a0eb7a91515b5813c90fc2400b1
I Kadek Adi Gunawan <adi_gunawan@live.com>
2022-06-03 05:58:05 +0000

    install mantine core

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
+import type { ColorScheme } from '@mantine/core'
+import { ColorSchemeProvider, MantineProvider } from '@mantine/core'
+import { ModalsProvider } from '@mantine/modals'
 import LoadingOrError from 'components/LoadingOrError'
 import type { ReactElement } from 'react'
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const Gallery = lazy(async () => import('pages/Gallery'))
 const Details = lazy(async () => import('pages/Details'))
 
 export default function App(): ReactElement {
+	const [colorScheme, setColorScheme] = useState<ColorScheme>('light')
+	const toggleColorScheme = (value?: ColorScheme): void =>
+		setColorScheme(value ?? (colorScheme === 'dark' ? 'light' : 'dark'))
 	return (
-		<BrowserRouter>
-			<Suspense fallback={<LoadingOrError />}>
-				<Routes>
-					<Route path='/' element={<Gallery />} />
-					<Route path=':fruitName' element={<Details />} />
-				</Routes>
-			</Suspense>
-		</BrowserRouter>
+		<ColorSchemeProvider
+			colorScheme={colorScheme}
+			toggleColorScheme={toggleColorScheme}
+		>
+			<MantineProvider theme={{ colorScheme }}>
+				<ModalsProvider>
+					<BrowserRouter>
+						<Suspense fallback={<LoadingOrError />}>
+							<Routes>
+								<Route path='/' element={<Gallery />} />
+								<Route path=':fruitName' element={<Details />} />
+							</Routes>
+						</Suspense>
+					</BrowserRouter>
+				</ModalsProvider>
+			</MantineProvider>
+		</ColorSchemeProvider>
 	)
 }
